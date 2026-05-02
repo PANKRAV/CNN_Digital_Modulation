63497cd9d4dd7e54d3e6bb897213a93dfacb9964
PANKRAV <96988405+PANKRAV@users.noreply.github.com>
2026-05-02 15:58:01 +0300

    dior8osi sto requirements

--- setup.bat
+++
@@ -1,9 +0,0 @@
-chdir %~dp0
-git clone https://github.com/PANKRAV/CNN_Digital_Modulation.git
-chdir CNN_Digital_Modulation
-REM python -m pip.exe install venv
-python -m venv ergasia --prompt vlakas-xristos
-ergasia\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-cmd /k
