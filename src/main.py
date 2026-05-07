539a3cb7390a2487a2d822e1bcfe20207a90bb36
PANKRAV <96988405+PANKRAV@users.noreply.github.com>
2026-05-07 15:38:18 +0300

    meriki dior8osi tou cnn

--- src/main.py
+++ src/main.py
@@ -10,17 +10,22 @@ from modules.utility import timeit
 
 
 class CNN(nn.Module):
+    rel = nn.ReLU
     def __init__(self):
         super(CNN, self).__init__()
 
         #1->2
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.rel1 = nn.ReLU()       
+        self.bn1 = nn.BatchNorm2d(16)        
         self.conv2 = nn.Conv2d(16, 24, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(24)
-        self.rel2 = nn.ReLU()
-        self.layer1 = self.conv1(self.bn1(self.rel1(self.conv2(self.bn2(self.rel2)))))
+        self.block1 = nn.Sequential(self.conv1,
+                                    CNN.rel,
+                                    self.bn1,
+                                    self.conv2,
+                                    CNN.rel,
+                                    self.bn2)
+
 
         #3
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -28,11 +33,15 @@ class CNN(nn.Module):
         #4->5
         self.conv3 = nn.Conv2d(24, 24, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(24)
-        self.rel3 = nn.ReLU()       
         self.conv4 = nn.Conv2d(24, 32, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(32)
-        self.rel4 = nn.ReLU()
-        self.layer2 = self.conv3(self.bn3(self.rel3(self.conv4(self.bn4(self.rel4)))))
+        self.block2 = nn.Sequential(self.pool1,
+                                    self.conv3,
+                                    CNN.rel,
+                                    self.bn3,
+                                    self.conv4,
+                                    CNN.rel,
+                                    self.bn4)        
 
         #6
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -40,23 +49,30 @@ class CNN(nn.Module):
         #7->8
         self.conv5 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.bn5 = nn.BatchNorm2d(32)
-        self.rel5 = nn.ReLU()       
         self.conv6 = nn.Conv2d(32, 48, kernel_size=3, stride=1, padding=1)
         self.bn6 = nn.BatchNorm2d(48)
-        self.rel6 = nn.ReLU()
-        self.layer3 = self.conv5(self.bn5(self.rel5(self.conv6(self.bn6(self.rel6)))))
-
+        self.block3 = nn.Sequential(self.pool2,
+                                    self.conv5,
+                                    CNN.rel,
+                                    self.bn5,
+                                    self.conv6,
+                                    CNN.rel,
+                                    self.bn6)
         #9
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         #10->11
         self.conv7 = nn.Conv2d(48, 48, kernel_size=3, stride=1, padding=1)
         self.bn7 = nn.BatchNorm2d(48)
-        self.rel7 = nn.ReLU()       
         self.conv8 = nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=1)
         self.bn8 = nn.BatchNorm2d(64)
-        self.rel8 = nn.ReLU()
-        self.layer4 = self.conv7(self.bn7(self.rel7(self.conv8(self.bn8(self.rel8)))))
+        self.block4 = nn.Sequential(self.pool3,
+                                    self.conv7,
+                                    CNN.rel,
+                                    self.bn7,
+                                    self.conv8,
+                                    CNN.rel,
+                                    self.bn8)
 
         #12
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -64,11 +80,15 @@ class CNN(nn.Module):
         #13->14
         self.conv9 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.bn9 = nn.BatchNorm2d(64)
-        self.rel9 = nn.ReLU()       
         self.conv10 = nn.Conv2d(64, 96, kernel_size=3, stride=1, padding=1)
         self.bn10 = nn.BatchNorm2d(96)
-        self.rel10 = nn.ReLU()
-        self.layer5 = self.conv9(self.bn9(self.rel9(self.conv10(self.bn10(self.rel10)))))
+        self.block5 = nn.Sequential(self.pool4,
+                                    self.conv9,
+                                    CNN.rel,
+                                    self.bn9,
+                                    self.conv10,
+                                    CNN.rel,
+                                    self.bn10)
 
         #15
         self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -76,24 +96,29 @@ class CNN(nn.Module):
         #16->17
         self.conv11 = nn.Conv2d(96, 96, kernel_size=3, stride=1, padding=1)
         self.bn11 = nn.BatchNorm2d(96)
-        self.rel11 = nn.ReLU()       
         self.conv12 = nn.Conv2d(96, 128, kernel_size=3, stride=1, padding=1)
         self.bn12 = nn.BatchNorm2d(128)
-        self.rel12 = nn.ReLU()
-        self.layer6 = self.conv11(self.bn11(self.rel11(self.conv12(self.bn12(self.rel12)))))
+        self.block6 = nn.Sequential(self.pool5,
+                                    self.conv11,
+                                    CNN.rel,
+                                    self.bn11,
+                                    self.conv12,
+                                    CNN.rel,
+                                    self.bn12)
 
         #18
-        self.pool6 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.Avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
         
         #19->20
         self.fc1 = nn.Linear(32 * 28 * 28, out_features=32 * 28 * 28)
         self.fc2 = nn.Linear(32 * 28 * 28, 15)
         self.full_control = self.fc1(self.fc2)
         #21
-        self.pool7 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        #22
         self.softmax = nn.Softmax2d()
+        self.output_block = nn.Sequential(self.Avg_pool,
+                                          self.fc1,
+                                          self.fc2,
+                                          self.softmax)
         
 
 
@@ -131,7 +156,7 @@ class CNN(nn.Module):
 
 
 def main(*args, **kwargs)-> None :
-    ...
+    CNN()
 
 
 
